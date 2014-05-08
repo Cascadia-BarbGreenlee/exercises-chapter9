8c3ee1e81b39959f948ae1a4c5658b51491d806f
Barb <gobaxter@gmail.com>
2014-05-07 17:48:03 -0700

    test

---
+++ LMAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LMAAppDelegate.h
+//  Hello World!
+//
+//  Created by Greenlee, Barb on 5/7/14.
+//  Copyright (c) 2014 Greenlee, Barb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LMAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
